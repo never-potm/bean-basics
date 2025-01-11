10e56e7e682f3fbb883715d140a1e5ac6aafffc8
Suraj Gusain <suraj.gusain15@gmail.com>
2025-01-11 17:43:01 +0530

    created a bean using contructor injection

---
+++ src/main/java/com/constructur/injection/Car.java
@@ -0,0 +1,13 @@
+package com.constructur.injection;
+
+public class Car {
+	private Specification specification;
+
+	public Car(Specification specification) {
+		this.specification = specification;
+	}
+
+	public void displayDetails() {
+		System.out.println("Car details: " + specification.toString());
+	}
+}
